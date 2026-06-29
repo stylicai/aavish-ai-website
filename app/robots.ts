918c18e6f7723c4c481fc2a3295f44b1dea0090c
stylicai <aavishailab@gmail.com>
2026-06-29 15:12:47 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,7 @@ import { site } from "@/lib/site-data";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", Disallow: "/" },
+    rules: { userAgent: "*", disallow: "/" },
     sitemap: `${site.url}/sitemap.xml`,
     host: site.url
   };
